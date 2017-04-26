092fba1e7ce4df8f66d796df8ce9aa0977dd577e
Asbjørn <Asbjrn@DESKTOP-MBFP8OQ>
2017-04-26 13:07:29 +0200

    CustomerFactory

--- Business/CustomerFactory.cs
+++ Business/CustomerFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DomainLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace Business
 {
     class CustomerFactory
     {
+        public void CreateCustomer(string name, string email, int phone, string address, int zip, string city, int cvr)
+        {
+            Customer newCustomer = new Customer(name, email, phone, address, zip, city, cvr);
 
+            
+        }
     }
 }
