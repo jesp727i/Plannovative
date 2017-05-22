e3cb71bb60ec05c46f7a3f5d49e05534c46f657b
Asbjørn <Asbjrn@DESKTOP-MBFP8OQ>
2017-05-22 14:26:39 +0200

    Noget View crap

---
+++ Plannovative/View/ExistingJobView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DomainLayer;
+
+namespace UserInterfaceLayer.View
+{
+    /// <summary>
+    /// Interaction logic for ExistingJobView.xaml
+    /// </summary>
+    public partial class ExistingJobView : Window
+    {
+        public ExistingJobView()
+        {
+            InitializeComponent();
+        }
+
+
+        public ExistingJobView(Customer cust)
+        {
+            
+        }
+    }
+}
